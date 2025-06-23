4cfc35ed0bca4336c5c325f1e4d8eb8f66f76016
Allan Correa <134990755+AllanC2002@users.noreply.github.com>
2025-06-23 14:39:49 -0500

    fix: issues with the worhflow

--- main.py
+++ main.py
@@ -37,4 +37,4 @@ def delete_account():
     return jsonify(response), code
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080, debug=True)
+    app.run(host="0.0.0.0", port=8081, debug=True)
